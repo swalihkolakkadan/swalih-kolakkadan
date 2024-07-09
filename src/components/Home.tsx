0d6f30ec6935a4504c47b7423e25c7d9ec7ad9b1
swalihkolakkadan <swalihkolakkadan@gmail.com>
2024-07-09 20:34:49 +0530

    Font and style corrections

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -33,25 +33,23 @@ const Home = () => {
   };
   return (
     <div className="flex flex-col md:flex-row flex-grow">
-      <div className="flex-1 max-w-xl mt-4 md:mt-32 ml-4 md:ml-24">
-        <div className="font-interRegular text-amber-700 dark:text-amber-100 pt-6 transition duration-700">
+      <div className="flex-1 max-w-xl mt-4 md:mt-52 ml-4 md:ml-24">
+        <div className=" text-amber-700 dark:text-amber-100 pt-6 transition duration-700">
           Hello <span className="inline-block scale-x-[-1]"> 👋</span>
         </div>
-        <div className="font-interBold text-3xl	mt-4">
-          {`I'm ${profile.name}`}
-        </div>
-        <div className="font-interLight pt-6">{profile.aboutMe}</div>
+        <div className="font-semibold text-3xl	mt-2">{`I'm ${profile.name}`}</div>
+        <div className=" pt-4">{profile.aboutMe}</div>
         <div className="mt-8 flex">
           <a
             href="https://drive.google.com/file/d/1CoHpjU0Hh8HXJOcqGlRX6AmD6xEwlu7O/view?usp=sharing"
             target="_blank"
           >
-            <button className="bg-amber-600 text-slate-200 dark:bg-amber-200 dark:text-slate-900 w-36 h-12 font-semibold rounded-md px-4 hover:text-amber-200 dark:hover:text-amber-500 transition duration-500">
+            <button className="bg-amber-600 text-slate-200 dark:bg-amber-200 dark:text-slate-900 w-36 h-12 font-semibold rounded-md px-4 hover:text-amber-200 dark:hover:text-amber-500 transition duration-300">
               View Resume
             </button>
           </a>
           <button
-            className=" bg-amber-600 text-slate-200 dark:bg-amber-200 dark:text-slate-900 ml-8 w-36 h-12  font-semibold rounded-md hover:text-amber-200 dark:hover:text-amber-500 transition duration-500"
+            className=" bg-amber-600 text-slate-200 dark:bg-amber-200 dark:text-slate-900 ml-8 w-36 h-12  font-semibold rounded-md hover:text-amber-200 dark:hover:text-amber-500 transition duration-300"
             onClick={toggleTheme}
           >
             <FontAwesomeIcon
