657c3216bfc4602ce05eca652e946295d6af2c77
swalihkolakkadan <swalihkolakkadan@gmail.com>
2024-07-13 11:50:01 +0530

    Added inter font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import Home from "./Home";
 
 const Layout = ({ children }: any) => {
   return (
-    <div className="bg-neutral-300 dark:bg-neutral-800 text-fcolor-light dark:text-gray-200 flex flex-col min-h-screen transition duration-700">
+    <div className="bg-neutral-300 dark:bg-neutral-800 text-fcolor-light dark:text-gray-200 flex flex-col min-h-screen transition duration-700 font-inter">
       <main className="grid grid-cols-1 lg:grid-cols-2 gap-2 flex-grow px-6 py-12">
         <Home />
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
       spacing: {
         "2px": "2px",
       },
+      fontFamily: {
+        inter: ["inter"],
+      },
     },
   },
   plugins: [],
